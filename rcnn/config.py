4b0de846b8ad6719319f162c2552876edd902844
qilei123 <qileimail123@gmail.com>
2018-11-29 13:23:53 -0500

    dfadf

--- rcnn/config.py
+++ rcnn/config.py
@@ -162,6 +162,7 @@ dataset.Retina.ANCHOR_SCALES = (8,)
 dataset.Retina.ANCHOR_RATIOS = (0.5, 1, 2)
 dataset.Retina.NUM_ANCHORS = len(dataset.Retina.ANCHOR_SCALES) * len(dataset.Retina.ANCHOR_RATIOS)
 '''
+config.TRAIN.SCALE = True
 dataset.Retina.dataset_path = '/home/qileimail123/data0/RetinaImg/ROP_COCO'
 dataset.Retina.NUM_CLASSES = 12
 dataset.Retina.CLASS_ID = [0, 1, 2, 3, 4, 5, 6, 7, 8,9,10,11]
@@ -170,6 +171,7 @@ dataset.Retina.CLASSES = ['Background','Macula','OpticDisk','Laser Photocoagulat
                     'artifact','Stage 3: ridge with neovascularization','proliferation', \
                     'Retina detachment','Carcinoma']
 '''
+config.TRAIN.SCALE = False
 dataset.Retina.dataset_path = '/home/qileimail123/data0/RetinaImg/DR_COCO'
 dataset.Retina.NUM_CLASSES = 11
 dataset.Retina.CLASS_ID = [0, 1, 2, 3, 4, 5, 6, 7, 8,9,10]
