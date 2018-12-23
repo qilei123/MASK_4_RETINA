7af1a8e000347831c7e19b505c50438fda754dc0
qilei123 <qileimail123@gmail.com>
2018-12-23 16:10:02 -0500

    fda

--- rcnn/symbol/symbol_mask_fpn_dcn_v1.py
+++ rcnn/symbol/symbol_mask_fpn_dcn_v1.py
@@ -7,7 +7,7 @@ eps = 2e-5
 use_global_stats = True
 workspace = 512
 res_deps = {'50': (3, 4, 6, 3), '101': (3, 4, 23, 3), '152': (3, 8, 36, 3), '200': (3, 24, 36, 3)}
-units = res_deps['101']
+units = res_deps['50']
 filter_list = [256, 512, 1024, 2048]
 
 def residual_unit(data, num_filter, stride, dim_match, name):
